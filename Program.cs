601a82acf1a1fc7e6f6197e43c178a0589501683
Pratama-Raka <pratamapoetra14@gmail.com>
2026-04-14 08:15:04 +0700

    Menambahkan class Watchlist_103022400008

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@
         public static void Main(string[] args) {
             var dataFilm = new FilmFavorite_103022400008();
             dataFilm.ReadJSON();
+
+            var wl = new Watchlist_103022400008();
+            wl.ReadJSON();
         }
     }
 }
---
+++ Watchlist_103022400008.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MODUL7_GGS
+{
+
+    public class movieStruc {
+        [JsonInclude]
+        public string id;
+        [JsonInclude]
+        public int year;
+        [JsonInclude]
+        public string genre;
+        [JsonInclude]
+        public double rating;
+    }
+    public class Watchlist_103022400008
+    {
+        [JsonInclude]
+        public string watchList;
+        [JsonInclude]
+        public string createdBy;
+        [JsonInclude]
+        public List<movieStruc> movies;
+
+        public void ReadJSON() {
+            string fileJSON = "jurnal7_2_103022400008.json";
+            string stringifyJson = File.ReadAllText(fileJSON);
+            var data = JsonSerializer.Deserialize<Watchlist_103022400008>(stringifyJson)!;
+
+
+                Console.WriteLine(data.watchList);
+                Console.WriteLine(data.createdBy);
+                foreach (var item in data.movies) {
+                    Console.WriteLine("Data Film : \nId : " + item.id +  " \nTitle : " + item.year.ToString() + " \nDirector: " + item.genre + " \nRating"
+                    + item.rating.ToString());
+                }
+            
+        }
+    }
+}
